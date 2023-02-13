e6ede884502783bd6cdb1c7866710c1a5b424bb0
shameer2762 <shaikshameer2762@gmail.com>
2023-02-13 12:41:24 +0530

    first commit at temp

---
+++ js/script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const hidden1El = document.querySelector('.hidden-1')
+const hidden2El = document.querySelector('.hidden-2')
+const hidden3El = document.querySelector('.hidden-3')
+const hidden4El = document.querySelector('.hidden-4')
+
+const current1El = document.querySelector('.current--1')
+const current2El = document.querySelector('.current--2')
+const current3El = document.querySelector('.current--3')
+const current4El = document.querySelector('.current--4')
+
+const player1El = document.querySelector('.player--1')
+const player2El = document.querySelector('.player--2')
+const player3El = document.querySelector('.player--3')
+const player4El = document.querySelector('.player--4')
+
+// hidden1El.classList.add('roll-and-hold-1')
+// current1El.classList.add('current--1-hidden')
+// player1El.classList.add('player--1-hidden')
+
+// // hidden2El.classList.add('roll-and-hold-2')
+// // current2El.classList.add('current--2-hidden')
+// // player2El.classList.add('player--2-hidden')
+
+// hidden3El.classList.add('roll-and-hold-3')
+// current3El.classList.add('current--3-hidden')
+// player3El.classList.add('player--3-hidden')
+
+// hidden4El.classList.add('roll-and-hold-4')
+// current4El.classList.add('current--4-hidden')
+// player4El.classList.add('player--4-hidden')
